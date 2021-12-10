8ffa03e6b076d9d3e58eca89df30e9b7072de607
JSisques <javierplazasisques@gmail.com>
2021-12-10 20:23:17 +0100

    Root commit

---
+++ Birthday-Paradox.py
@@ -0,0 +1,14 @@
+def main():
+
+    DAYS_OF_YEAR = 365
+    currentDay = DAYS_OF_YEAR
+
+    numberOfPeople = int(input("Introduce el número de personas del grupo: "))
+
+    # 100% - % de no cumplir años el mismo dia x personas
+
+    for i in range(0, numberOfPeople):
+        pass
+
+if __name__ == '__main__':
+    main()
